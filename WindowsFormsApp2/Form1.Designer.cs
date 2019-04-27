6174efbb7cc2f2aa98f0e6c31e87531d5b944624
Shkelqim Maxharraj <shkelqimimaxharraj@gmail.com>
2019-04-27 19:13:27 +0200

    Adding the first part of algorithem
    The algorithem is able to decode only if shift is one letter

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uploadFileBtn = new System.Windows.Forms.Button();
+            this.filePath = new System.Windows.Forms.TextBox();
+            this.loadFileBtn = new System.Windows.Forms.Button();
+            this.decryptFileBtn = new System.Windows.Forms.Button();
+            this.saveFileBtn = new System.Windows.Forms.Button();
+            this.encryptedFile = new System.Windows.Forms.RichTextBox();
+            this.decryptedFile = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // uploadFileBtn
+            // 
+            this.uploadFileBtn.Location = new System.Drawing.Point(33, 27);
+            this.uploadFileBtn.Name = "uploadFileBtn";
+            this.uploadFileBtn.Size = new System.Drawing.Size(224, 53);
+            this.uploadFileBtn.TabIndex = 0;
+            this.uploadFileBtn.Text = "Upload a file";
+            this.uploadFileBtn.UseVisualStyleBackColor = true;
+            this.uploadFileBtn.Click += new System.EventHandler(this.UploadFileBtn_Click);
+            // 
+            // filePath
+            // 
+            this.filePath.Location = new System.Drawing.Point(33, 102);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(224, 20);
+            this.filePath.TabIndex = 1;
+            // 
+            // loadFileBtn
+            // 
+            this.loadFileBtn.Location = new System.Drawing.Point(33, 138);
+            this.loadFileBtn.Name = "loadFileBtn";
+            this.loadFileBtn.Size = new System.Drawing.Size(224, 37);
+            this.loadFileBtn.TabIndex = 2;
+            this.loadFileBtn.Text = "Load File";
+            this.loadFileBtn.UseVisualStyleBackColor = true;
+            this.loadFileBtn.Click += new System.EventHandler(this.LoadFileBtn_Click);
+            // 
+            // decryptFileBtn
+            // 
+            this.decryptFileBtn.Location = new System.Drawing.Point(445, 27);
+            this.decryptFileBtn.Name = "decryptFileBtn";
+            this.decryptFileBtn.Size = new System.Drawing.Size(291, 53);
+            this.decryptFileBtn.TabIndex = 3;
+            this.decryptFileBtn.Text = "Decrypt file";
+            this.decryptFileBtn.UseVisualStyleBackColor = true;
+            this.decryptFileBtn.Click += new System.EventHandler(this.DecryptFileBtn_Click);
+            // 
+            // saveFileBtn
+            // 
+            this.saveFileBtn.Location = new System.Drawing.Point(534, 319);
+            this.saveFileBtn.Name = "saveFileBtn";
+            this.saveFileBtn.Size = new System.Drawing.Size(202, 38);
+            this.saveFileBtn.TabIndex = 6;
+            this.saveFileBtn.Text = "Save File";
+            this.saveFileBtn.UseVisualStyleBackColor = true;
+            // 
+            // encryptedFile
+            // 
+            this.encryptedFile.Location = new System.Drawing.Point(33, 205);
+            this.encryptedFile.Name = "encryptedFile";
+            this.encryptedFile.Size = new System.Drawing.Size(224, 152);
+            this.encryptedFile.TabIndex = 7;
+            this.encryptedFile.Text = "";
+            this.encryptedFile.TextChanged += new System.EventHandler(this.RichTextBox1_TextChanged);
+            // 
+            // decryptedFile
+            // 
+            this.decryptedFile.Location = new System.Drawing.Point(445, 102);
+            this.decryptedFile.Name = "decryptedFile";
+            this.decryptedFile.Size = new System.Drawing.Size(291, 176);
+            this.decryptedFile.TabIndex = 8;
+            this.decryptedFile.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.decryptedFile);
+            this.Controls.Add(this.encryptedFile);
+            this.Controls.Add(this.saveFileBtn);
+            this.Controls.Add(this.decryptFileBtn);
+            this.Controls.Add(this.loadFileBtn);
+            this.Controls.Add(this.filePath);
+            this.Controls.Add(this.uploadFileBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uploadFileBtn;
+        private System.Windows.Forms.TextBox filePath;
+        private System.Windows.Forms.Button loadFileBtn;
+        private System.Windows.Forms.Button decryptFileBtn;
+        private System.Windows.Forms.Button saveFileBtn;
+        private System.Windows.Forms.RichTextBox encryptedFile;
+        private System.Windows.Forms.RichTextBox decryptedFile;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DecryptFileBtn_Click(object sender, EventArgs e)
+        {
+            char Cipher(char ch, int key)
+            {
+                if (!char.IsLetter(ch))
+                    return ch;
+
+                char offset = char.IsUpper(ch) ? 'A' : 'a';
+                return (char)((((ch + key) - offset) % 26) + offset);
+            }
+
+            string Encipher(string input, int key)
+            {
+                string output = string.Empty;
+
+                foreach (char ch in input)
+                    output += Cipher(ch, key);
+
+                return output;
+            }
+            string textCezar = System.IO.File.ReadAllText(filePath.Text);
+
+            if (encryptedFile.Text.Contains(" OF ") == true || encryptedFile.Text.Contains(" UT "))
+            {
+                int shtyerja = 1;
+
+
+                decryptedFile.Text = Encipher(textCezar, 26-int);
+
+            }
+            else if (...)
+            {
+               ...
+
+            }
+        }
+
+        private void UploadFileBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog browse = new OpenFileDialog();
+                browse.ShowDialog();
+                if (browse.ShowDialog() == DialogResult.OK)
+                {
+                    filePath.Text = browse.FileName;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void LoadFileBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                try
+                {
+                    string lines = System.IO.File.ReadAllText(filePath.Text);
+
+                    encryptedFile.Text = lines;
+                }
+                catch
+                {
+
+                }
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
