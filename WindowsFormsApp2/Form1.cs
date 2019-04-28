d1bc53bfee5c7f74f63a125c7722788af29ddf2d
NatyraBlakaj <35508174+NatyraBlakaj@users.noreply.github.com>
2019-04-28 14:29:24 +0200

    Shtimi i 25 rasteve te tjera per dekodim
    Tani algoritmi eshte i gatshem te dekodoj qdo tekst ne gjuhen shqipe qe i permban disa fjale. (Mbetet te aktivizohet edhe buttoni Save File) i cili e mbishkruan fajllin e enkoduar me fajllin e ri  te dekoduar

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -42,20 +42,172 @@ namespace WindowsFormsApp2
 
                 return output;
             }
-            string textCezar = System.IO.File.ReadAllText(filePath.Text);
+            string textCezar = System.IO.File.ReadAllText(filePath.Text).ToUpper();
 
             if (encryptedFile.Text.Contains(" OF ") == true || encryptedFile.Text.Contains(" UT "))
             {
                 int shtyerja = 1;
 
 
-                decryptedFile.Text = Encipher(textCezar, 26-int);
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja );
 
             }
-            else if (...)
+            else if (encryptedFile.Text.Contains(" PG ") == true || encryptedFile.Text.Contains(" VG "))
             {
-               ...
+                int shtyerja = 2;
 
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+
+            }
+            else if (encryptedFile.Text.Contains(" QH ") == true || encryptedFile.Text.Contains(" WH "))
+            {
+                int shtyerja = 3;
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+
+            }
+            else if (encryptedFile.Text.Contains(" RI ") == true || encryptedFile.Text.Contains(" XI "))
+            {
+
+                int shtyerja = 4;
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+            }
+            else if (encryptedFile.Text.Contains(" SJ ") == true || encryptedFile.Text.Contains(" YJ "))
+            {
+                int shtyerja = 5;
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+            }
+            else if (encryptedFile.Text.Contains(" TK ") == true || encryptedFile.Text.Contains(" ZK "))
+            {
+                int shtyerja = 6;
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+            }
+            else if (encryptedFile.Text.Contains(" UL ") == true || encryptedFile.Text.Contains(" AL "))
+            {
+                int shtyerja = 7;
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+
+            }
+            else if (encryptedFile.Text.Contains(" VM ") == true || encryptedFile.Text.Contains(" BM "))
+            {
+                int shtyerja = 8;
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+
+            }
+            else if (encryptedFile.Text.Contains(" WN ") == true || encryptedFile.Text.Contains(" CN "))
+            {
+                int shtyerja = 9;
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+            }
+            else if (encryptedFile.Text.Contains(" XO ") == true || encryptedFile.Text.Contains(" DO "))
+            {
+                int shtyerja = 10;
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+            }
+            else if (encryptedFile.Text.Contains(" YP ") == true || encryptedFile.Text.Contains(" EP "))
+            {
+                int shtyerja = 11;
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+            }
+            else if (encryptedFile.Text.Contains(" ZQ ") == true || encryptedFile.Text.Contains(" FQ "))
+            {
+                int shtyerja = 12;
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+            }
+            else if (encryptedFile.Text.Contains(" AR ") == true || encryptedFile.Text.Contains(" GR "))
+            {
+                int shtyerja = 13;
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+            }
+            else if (encryptedFile.Text.Contains(" BS ") == true || encryptedFile.Text.Contains(" HS "))
+            {
+                int shtyerja = 14;
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+            }
+            else if (encryptedFile.Text.Contains(" CT ") == true || encryptedFile.Text.Contains(" IT "))
+            {
+                int shtyerja = 15;
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+            }
+            else if (encryptedFile.Text.Contains(" DU ") == true || encryptedFile.Text.Contains(" JU "))
+            {
+                int shtyerja = 16;
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+            }
+            else if (encryptedFile.Text.Contains(" EV ") == true || encryptedFile.Text.Contains(" KV "))
+            {
+                int shtyerja = 17;
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+            }
+            else if (encryptedFile.Text.Contains(" FW ") == true || encryptedFile.Text.Contains(" LW "))
+            {
+                int shtyerja = 18;
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+            }
+            else if (encryptedFile.Text.Contains(" GX ") == true || encryptedFile.Text.Contains(" MX "))
+            {
+
+                int shtyerja = 19;
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+            }
+            else if (encryptedFile.Text.Contains(" HY ") == true || encryptedFile.Text.Contains(" NY "))
+            {
+                int shtyerja = 20;
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+            }
+            else if (encryptedFile.Text.Contains(" IZ ") == true || encryptedFile.Text.Contains(" OZ "))
+            {
+                int shtyerja = 21;
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+            }
+            else if (encryptedFile.Text.Contains(" JA ") == true || encryptedFile.Text.Contains(" PA "))
+            {
+                int shtyerja = 22;
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+            }
+            else if (encryptedFile.Text.Contains(" KB ") == true || encryptedFile.Text.Contains(" QB "))
+            {
+                int shtyerja = 23;
+
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+            }
+            else if (encryptedFile.Text.Contains(" LC ") == true || encryptedFile.Text.Contains(" RC "))
+            {
+                int shtyerja = 24;
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+            }
+            else if (encryptedFile.Text.Contains(" MD ") == true || encryptedFile.Text.Contains(" SD "))
+            {
+                int shtyerja = 25;
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
+            }
+            else if (encryptedFile.Text.Contains(" NE ") == true || encryptedFile.Text.Contains(" TE "))
+            {
+                int shtyerja = 26;
+
+                decryptedFile.Text = Encipher(textCezar, 26 - shtyerja);
             }
         }
 
@@ -84,7 +236,7 @@ namespace WindowsFormsApp2
                 {
                     string lines = System.IO.File.ReadAllText(filePath.Text);
 
-                    encryptedFile.Text = lines;
+                    encryptedFile.Text = lines.ToUpper();
                 }
                 catch
                 {
